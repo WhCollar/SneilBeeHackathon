1c1a70af01d83ab1d740fa101a2816f308d1af22
Anastasia <nastya-kotikm@yandex.ru>
2022-10-07 21:17:00 +0300

    Добавлены файлы стора

---
+++ client/src/store/user/reducer.js
@@ -0,0 +1,12 @@
+import {
+} from './actionsTypes';
+
+const initialState = {
+};
+
+export default function listReducer(state = initialState, action) {
+  switch (action.type) {
+
+    default: return state;
+  }
+}
